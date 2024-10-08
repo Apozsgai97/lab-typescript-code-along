70fb70aa4acaa0766f323c7b8eedc36477836a45
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-08 15:36:45 +0200

    uppercaseNames works

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,21 @@
 import { deepEqual } from "node:assert/strict";
 import test from "node:test";
 
-test("It works", () => {
-  deepEqual(1,1);
+/*const data = [
+{id: "e3cd129c-502f-4fdc-bc9b-ac926b5955cc", name: "Elton Johnson" },
+{id: "e3cd129c-502f-4fdc-bc9b-ac926b5955c2", name:"Id Po"}
+];*/
+
+type Person = {
+ id: string;
+ name: string;
+}
+
+function uppercaseNames(names: Person[]) {
+ return [];
+}
+
+test("UppercaseNames works with empty array", () => {
+  const result = uppercaseNames([]);
+ deepEqual(result,[]);
 });
